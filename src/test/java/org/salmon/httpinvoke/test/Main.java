23940b5bf07ef0abdecf6322f5914cb21143d744
524220752 <524220752@qq.com>
2015-09-06 20:32:40 +0800

    增加幂等性重试

--- src/test/java/org/salmon/httpinvoke/test/Main.java
+++ src/test/java/org/salmon/httpinvoke/test/Main.java
@@ -16,21 +16,30 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		PoolingHttpClientImpl customHttpClient = new PoolingHttpClientImpl();
 
-//		Map<String, String> dnsMap = new HashMap<String, String>();
-//		dnsMap.put("www.baidu.com", "180.97.33.107");
-//		dnsMap.put("www.sina.com", "202.102.75.147");
-//		dnsMap.put("ddfsdfasdfasdf", "127.0.0.1");
-//		dnsMap.put("www.wuwenyu.com", "123.57.3.169");
-//		customHttpClient.setDnsMap(dnsMap); 
+		/**
+		 * 域名映射功能
+		 */
+		Map<String, String> dnsMap = new HashMap<String, String>();
+		dnsMap.put("www.baidu.com", "180.97.33.107");
+		dnsMap.put("www.sina.com", "202.102.75.147");
+		dnsMap.put("ddfsdfasdfasdf", "127.0.0.1");
+		dnsMap.put("www.wuwenyu.com", "123.57.3.169");
+		customHttpClient.setDnsMap(dnsMap); 
+		
+		/**
+		 * 幂等性重试
+		 */
+		customHttpClient.setMaxRetries(3);
+		 
 		customHttpClient.init();
 		
 		System.out.println(customHttpClient.get("https://www.baidu.com"));
 
-//		customHttpClient.get("http://www.baidu.com");
-//		customHttpClient.get("http://www.sina.com");
-//		customHttpClient.get("http://ddfsdfasdfasdf:8080");
-//		System.out.println(customHttpClient.get("http://123.57.3.169"));
-//		System.out.println(customHttpClient.getTotalPoolStats());
+		customHttpClient.get("http://www.baidu.com");
+		customHttpClient.get("http://www.sina.com");
+		customHttpClient.get("http://ddfsdfasdfasdf:8080");
+		System.out.println(customHttpClient.get("http://123.57.3.169"));
+		System.out.println(customHttpClient.getTotalPoolStats());
 	}
 
 }
